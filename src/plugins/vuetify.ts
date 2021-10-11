21e7721557af05d4f7312b270a6c6a5c8ad260dd
guzuro <guzurogmn@gmail.com>
2021-10-12 01:46:56 +0300

    icons

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import '@mdi/font/css/materialdesignicons.css'; // Ensure you are using css-loader
 
 Vue.use(Vuetify);
 
-const vuetify = new Vuetify({});
+const vuetify = new Vuetify({
+  icons: {
+    iconfont: 'mdi', // default - only for display purposes
+  },
+});
 export default vuetify;

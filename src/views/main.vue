607cce3824e8021043039679df04c6c5600e6009
guzuro <guzurogmn@gmail.com>
2021-10-12 00:41:19 +0300

    main page

---
+++ src/views/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    main page
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator';
+import Vue from 'vue';
+
+@Component
+export default class Main extends Vue {
+}
+</script>
+
+<style scoped>
+
+</style>

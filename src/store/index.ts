104d8f13d940d940bd5987a925de5b58e646082a
guzuro <guzurogmn@gmail.com>
2021-10-12 23:58:48 +0300

    add user module to state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,50 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import userModule from './modules/user/state';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    isAuth: false,
+    defaultUsers: [
+      {
+        login: 'customer',
+        password: 'customer',
+        profile: {
+          name: 'Jora',
+          phone: '+79994524548',
+          email: 'customer@email.ru',
+          company: 'OOO "STROIM DOMA"',
+          role: 'customer',
+          orders: [],
+        },
+      },
+      {
+        login: 'manager',
+        password: 'manager',
+        profile: {
+          name: 'Irina',
+          phone: '+789456321321',
+          email: 'manager@email.ru',
+          role: 'manager',
+        },
+      },
+      {
+        login: 'warehouse',
+        password: 'warehouse',
+        profile: {
+          name: 'Peters',
+          phone: '+746546546456',
+          email: 'warehouse@email.ru',
+          role: 'warehouse',
+        },
+      },
+    ],
+    userState: {},
   },
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    userModule,
+  },
 });
---
+++ src/store/modules/user/state.ts
@@ -0,0 +1,39 @@
+/* eslint no-shadow: ["error", { "allow": ["state"] }] */
+
+import { Commit } from 'vuex';
+
+export interface State {
+  isAuth: false;
+  userData: any;
+}
+
+const state: State = {
+  isAuth: false,
+  userData: {},
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations: {
+    SET_USER_TO_STORE: (state: State, value: any) => {
+      state.userData = value.profile;
+    },
+    SET_AUTH_STATE: (state: State, value: any) => {
+      state.isAuth = value;
+    },
+  },
+  actions: {
+    setUserToStore: (context: { commit: Commit }, value: any): void => {
+      context.commit('SET_USER_TO_STORE', value);
+    },
+    setAuthState: (context: { commit: Commit }, value: boolean): void => {
+      context.commit('SET_AUTH_STATE', value);
+    },
+  },
+  getters: {
+    isAuthenticated(state: State): boolean {
+      return state.isAuth;
+    },
+  },
+};

fbd3e295cb890e3293d92a09af925a6a0953cf3a
guzuro <guzurogmn@gmail.com>
2021-10-13 03:14:22 +0300

    created orders page

---
+++ src/views/orders.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="orders">
+    orders only login users
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator';
+import Vue from 'vue';
+
+@Component
+export default class Orders extends Vue {
+}
+</script>
+
+<style scoped>
+
+</style>
